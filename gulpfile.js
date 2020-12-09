fda0b6fc9acadde2d9717c363ae740ddde7839ca
sexfat <bryant6831@gmail.com>
2020-12-09 12:01:24 +0800

    add 所有任務

--- gulpfile.js
+++ gulpfile.js
@@ -27,25 +27,31 @@ function move() {
 exports.copyHtml = move //任務輸出
 exports.js = jstask
 exports.css = csstask
-exports.all = series(jstask, csstask);//串連
-exports.all2 =  parallel(jstask, csstask);//並行
+exports.all = series(jstask, csstask); //串連
+exports.all2 = parallel(jstask, csstask); //並行
 
 
 // 合併檔案
 const concat = require('gulp-concat');
+const cleanCSS = require('gulp-clean-css');
 
 function concatCss() {
     return src('css/*.css').pipe(concat('all.css')).pipe(dest('app/css'));
 }
 
+// 所有任務
+function concatall() {
+    return src('css/*.css').pipe(concat('all.css')).pipe(cleanCSS()).pipe(dest('app/css'));
+}
 exports.concat = concatCss; //任務輸出
 
+exports.allmission = concatall
 
 //壓縮css
-const cleanCSS = require('gulp-clean-css');
+
 
 function minify() {
-  return src('app/css/*.css').pipe(cleanCSS()).pipe(dest('app/css/mini'));
-} 
-
-exports.allcss = series(concatCss , minify);
+    return src('app/css/*.css').pipe(cleanCSS()).pipe(dest('app/css/mini'));
+}
+//先合併css 在壓縮css
+exports.allcss = series(concatCss, minify);

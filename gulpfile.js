ed1cb9efc211e7fe58c830e9e743fe81d320d9bf
sexfat <bryant6831@gmail.com>
2020-12-09 10:58:12 +0800

    add 串連並行

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,33 @@
-const {src , dest } = require('gulp');
-
-function jstask(cb){
-  console.log('打包js');
-  cb();
+const {
+    src,
+    dest,
+    series,
+    parallel
+} = require('gulp');
+//js 任務 
+function jstask(cb) {
+    console.log('打包js');
+    cb();
 }
-
-function csstask(cb){
+//css  任務
+function csstask(cb) {
     console.log('打包css');
     cb();
-  }
+}
 
-function move(){
+
+//  搬家的
+function move() {
     //src  來源
-   return src('index.html').pipe(dest('app/')) // dest 目的地
-}  
+    return src('index.html').pipe(dest('app/')) // dest 目的地
+}
+
+
+
+
 
 exports.copyHtml = move //任務輸出
-
 exports.js = jstask
 exports.css = csstask
-
-
-
-
+exports.all = series(jstask, csstask);//串連
+exports.all2 =  parallel(jstask, csstask);//並行

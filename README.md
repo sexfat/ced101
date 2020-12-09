32e9f5d01d0b6160fe48048275fa45746fcabd66
sexfat <bryant6831@gmail.com>
2020-12-09 10:46:08 +0800

    add

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # gulp講義
 
+- https://gulpjs.com/
+
 - https://hackmd.io/RETnNFtqRJmE4NBolrQCUQ
 - https://hackmd.io/iaDWtB3NRzmefW2IQltHJw
---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const {src , dest } = require('gulp');
+
+function jstask(cb){
+  console.log('打包js');
+  cb();
+}
+
+function csstask(cb){
+    console.log('打包css');
+    cb();
+  }
+
+function move(){
+    //src  來源       
+   return src('index.html').pipe(dest('app/')) // dest 目的地
+}  
+
+exports.copyHtml = move
+
+exports.js = jstask
+exports.css = csstask
+
+
+
+

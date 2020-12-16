7472efcd967809363b84a30d18c156c397bb83f4
sexfat <bryant6831@gmail.com>
2020-12-16 16:23:25 +0800

    add

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 import $ from 'jquery';
+import { TweenMax } from 'gsap';
 
 
-$('body').css('background-color' , 'green');
 
-console.log('webpack 安裝完成');
+$('body').css('background-color' , '#333');
+TweenMax.to('.box01' , 1 , {
+    x: 100,
+    y: 100,
+    rotation : 180
+})
+
+console.log('動畫執行 安裝完成');

746a581eaaf663a10073f85f9201ef6db350110c
sexfat <bryant6831@gmail.com>
2020-12-16 16:36:06 +0800

    add

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-import $ from 'jquery';
+// import $ from 'jquery';
 import { TweenMax } from 'gsap';
+import Vue from 'vue';
+
+
+new Vue({
+    el : '#app',
+    data : {
+       message : 'hello vue' 
+    }
+})
 
 
 
-$('body').css('background-color' , '#333');
+// $('body').css('background-color' , '#333');
 TweenMax.to('.box01' , 1 , {
     x: 100,
     y: 100,

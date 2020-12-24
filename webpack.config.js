ab017598c76d4148a79a065a0f9360ee493d9eae
sexfat <bryant6831@gmail.com>
2020-12-24 10:45:41 +0800

    add

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path'); 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/app.js',               // 入口文件
@@ -12,15 +13,24 @@ module.exports = {
             // 格式
             test: /\.(sass|scss|css)$/,
             //順序是由下到上 css > style
-            use: [
-                'style-loader',
+            use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                      publicPath: './dist'
+                    }
+                  },
+                // 'style-loader',
                 'css-loader',
                 'sass-loader'
             ],
         }]
 
     }, // 處裡對應模組
-    // plugins: [],             // 對應的插件
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "./style.css"
+        })
+    ],            // 對應的插件
     // devServer: {},           // 服務器配置
     mode: 'production'      // 開發模式配置 development  / production 產品上線
 }
